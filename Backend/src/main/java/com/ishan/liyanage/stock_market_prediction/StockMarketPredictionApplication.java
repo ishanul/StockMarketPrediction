4ecc51118b7d77f1030c008e736bdcf061bc38cd
Ishan Liyanage <ishanul@gmail.com>
2024-07-08 22:55:38 +0800

    Adding initial Java backend code

---
+++ Backend/src/main/java/com/ishan/liyanage/stock_market_prediction/StockMarketPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.ishan.liyanage.stock_market_prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketPredictionApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ishan/liyanage/stock_market_prediction/StockMarketPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan.liyanage.stock_market_prediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
