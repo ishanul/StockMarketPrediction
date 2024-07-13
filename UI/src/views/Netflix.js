cb399e98d6fee91a46bf0b114aa44dfb09c8da00
Ishan Liyanage <ishanul@gmail.com>
2024-07-13 11:46:39 +0800

    Adding Latest and Actual Price values in each page and individual charts

--- UI/src/views/Netflix.js
+++ UI/src/views/Netflix.js
@@ -99,6 +99,71 @@ function Dashboard(props) {
       ],
     };
   }
+  let data2 = (canvas) => {
+    let ctx = canvas.getContext("2d");
+
+    let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke.addColorStop(1, "rgba(29,140,248,0.2)");
+    gradientStroke.addColorStop(0.4, "rgba(29,140,248,0.0)");
+    gradientStroke.addColorStop(0, "rgba(29,140,248,0)"); //blue colors
+
+    return {
+      labels: chart3?.[0]?.indexes,
+      datasets: [
+        {
+          label: "Predicted History",
+          fill: true,
+          backgroundColor: gradientStroke,
+          borderColor: "#1f8ef1",
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 0.0,
+          pointBackgroundColor: "#1f8ef1",
+          pointBorderColor: "rgba(255,255,255,0)",
+          pointHoverBackgroundColor: "#1f8ef1",
+          pointBorderWidth: 20,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 15,
+          pointRadius: 4,
+          data: chart3?.[0]?.predicts,
+        },
+      ],
+    };
+  };
+
+  let data4 = (canvas) => {
+    let ctx = canvas.getContext("2d");
+
+    let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke.addColorStop(1, "rgba(66,134,121,0.15)");
+    gradientStroke.addColorStop(0.4, "rgba(66,134,121,0.0)"); //green colors
+    gradientStroke.addColorStop(0, "rgba(66,134,121,0)"); //green colors
+
+    return {
+      labels: chart3?.[0]?.indexes,
+      datasets: [
+        {
+          label: "Actual History",
+          fill: true,
+          backgroundColor: gradientStroke,
+          borderColor: "#00d6b4",
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 0.0,
+          pointBackgroundColor: "#00d6b4",
+          pointBorderColor: "rgba(255,255,255,0)",
+          pointHoverBackgroundColor: "#00d6b4",
+          pointBorderWidth: 20,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 15,
+          pointRadius: 4,
+          data: chart3?.[0]?.actuals,
+        },
+      ],
+    };
+  };
   const [bigChartData, setbigChartData] = React.useState("data1");
 
 useEffect(() => {
@@ -184,14 +249,41 @@ useEffect(() => {
           </Col>
         </Row>
         <Row>
-          <Col lg="4">
-           
+          <Col lg="6">
+            <Card className="card-chart">
+              <CardHeader>
+                <h5 className="card-category">Latest Predicted Price</h5>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-bell-55 text-info" /> ${chart3?.[0]?.predicts[chart3?.[0]?.predicts.length -1].toFixed(2)}
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={data2}
+                    options={chartExample2.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
           </Col>
-          <Col lg="4">
-            
-          </Col>
-          <Col lg="4">
-            
+          <Col lg="6">
+            <Card className="card-chart">
+              <CardHeader>
+                <h5 className="card-category">Latest Actual Price</h5>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-send text-success" /> ${chart3?.[0]?.actuals[chart3?.[0]?.actuals.length -1].toFixed(2)}
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={data4}
+                    options={chartExample4.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
           </Col>
         </Row>
         <Row>
